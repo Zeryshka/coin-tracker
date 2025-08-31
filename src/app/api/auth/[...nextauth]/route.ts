41df9e0ad2cb8fda2d0bf096aa4303c0612bcdd5
Zeryshka <maxim98sorokin@gmail.com>
2025-08-31 09:03:16 +0300

    Добавлен CredentialsProvider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,8 @@ import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prisma";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { compare } from "bcryptjs";
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -10,9 +12,57 @@ export const authOptions: AuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        login: { label: "Login или Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.login || !credentials?.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findFirst({
+          where: {
+            OR: [
+              { login: credentials.login },
+              { email: credentials.login },
+            ],
+          },
+        });
+
+        if (!user || !user.password) {
+          return null;
+        }
+
+        const isValid = await compare(credentials.password, user.password);
+        if (!isValid) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          login: user.login,
+          role: user.role,
+        };
+      },
+    }),
   ],
   pages: {
     signIn: "/auth/signin",
+  },
+  events: {
+    async createUser({ user }) {
+      if (!(user as any).login) {
+        await prisma.user.update({
+          where: { id: user.id },
+          data: { login: `Anonym${Date.now()}` },
+        });
+      }
+    },
   },
 };
 
